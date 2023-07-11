b7514b4179c0c4d07a81cff7b5d224582471d5ac
Andrea Vecino <andreavecino94@gmail.com>
2023-07-10 21:02:51 -0500

    Clase 10/07/2023

--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-intro',
@@ -35,12 +36,14 @@ export class IntroPage implements OnInit {
     }
   ]
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private storage: Storage) { }
 
   ngOnInit() {
   }
 
   close() {
+    console.log("estoy tratando de cerrar");
+    this.storage.set("introShow", true);
     this.router.navigateByUrl("/home");
   }
 
