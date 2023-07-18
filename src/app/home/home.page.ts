b7de212cba44f15f5ff14883ca669a9fc2090c58
Andrea Vecino <andreavecino94@gmail.com>
2023-07-17 21:55:33 -0500

    Clase 17/07/2023

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MusicService } from '../services/music.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  artists: any;
+  localArtists: any;
+  constructor(private musicService: MusicService) {}
 
-  constructor() {}
+  ionViewDidEnter(){
+
+    this.musicService.getArtists().then(listArtists => {
+      this.artists = listArtists;
+      console.log(this.artists.length);
+    })
+
+    this.localArtists = this.musicService.getArtistsFromJson();
+    console.log(this.localArtists.artists);
+  }
 
 }
