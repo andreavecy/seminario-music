666fd70fc5ba4ea3cd0f2f52539b066bfdf491a5
Andrea Vecino <andreavecino94@gmail.com>
2023-07-18 21:38:43 -0500

    Clase 18/07/2023

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { MusicService } from '../services/music.service';
+import { ModalController } from '@ionic/angular';
+import { SongsModalPage } from '../songs-modal/songs-modal.page';
 
 @Component({
   selector: 'app-home',
@@ -9,17 +11,43 @@ import { MusicService } from '../services/music.service';
 export class HomePage {
   artists: any;
   localArtists: any;
-  constructor(private musicService: MusicService) {}
+  song = {
+    name: ''
+  }
+  constructor(
+    private musicService: MusicService,
+    private modalController: ModalController
+    ) {}
 
   ionViewDidEnter(){
 
     this.musicService.getArtists().then(listArtists => {
       this.artists = listArtists;
-      console.log(this.artists.length);
+      //console.log(this.artists.length);
     })
 
     this.localArtists = this.musicService.getArtistsFromJson();
-    console.log(this.localArtists.artists);
+    this.localArtists = this.localArtists.artists
+    //console.log(this.localArtists.artists);
+  }
+
+  async showSongs(artist:any){
+    //console.log(artist);
+    const songs = await this.musicService.getArtistsTracks(artist.id);
+    //console.log(songs);
+    const modal = await this.modalController.create(
+      {
+        component: SongsModalPage,
+        componentProps: {
+          songs: songs,
+          artist: artist.name
+        }
+      }
+    );
+    modal.onDidDismiss().then( dataReturned => {
+      this.song = dataReturned.data;
+    });
+    return await modal.present();
   }
 
 }
